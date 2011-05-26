e2b0418659375d5744ee3d191c9d30904b709aba
Raju Bhosale <blackcloudandriod@gmail.com>
2011-05-26 21:58:19 +0530

    Done with Mostly static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,4 +11,7 @@ class PagesController < ApplicationController
     @title = "About"
   end
 	
+	def help
+    @title = "About"
+  end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,14 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+before(:each) do
+    #
+    # Define @base_title here.
+    @base_title="Ruby on Rails Tutorial Sample App";
+    #
+  end
+
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +20,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                        :content => @base_title + " | Home")
     end
   end
 
@@ -26,7 +34,7 @@ describe PagesController do
       get 'contact'
       response.should have_selector("title",
                         :content =>
-                          "Ruby on Rails Tutorial Sample App | Contact")
+                          @base_title + " | Contact")
     end
   end
 
@@ -40,8 +48,23 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",
                         :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
+                          @base_title + " | About")
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content =>
+                          @base_title + " | About")
+    end
+  end
+
 end
 
