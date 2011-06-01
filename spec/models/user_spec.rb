e2ea9620012bd57169f7e58639f27300b3404dce
Raju Bhosale <blackcloudandriod@gmail.com>
2011-06-01 19:03:37 +0530

    user model with password and user show page

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,39 @@ describe User do
   #pending "add some examples to (or delete) #{__FILE__}"
   
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com" }
+    @attr = { 	:name => "Example User",
+		   		:email => "user@example.com",
+    			:password => "foobar",
+    			:pasword_conformation => "foobar"				
+    		 }
   end
 
   it "should create a new instance given valid attributes" do
     User.create!(@attr)
   end
-
+  
+  it "should require a password " do
+  	User.new(@attr.merge(:password => "",:password_conformation => "")).
+  	should_not be_valid
+  end
+  
+  it "should require a matching password conformation " do
+  	User.new(@attr.merge(:password_conformation => "invalid")).
+  	should_not be_valid
+  end
+  
+  it "should reject short password " do 
+  	short ="a"*5
+  	hash =@attr.merge(:password =>short,:password_conformation =>short)
+  	User.new(hash).should_not be_valid
+  end
+  
+  it "should reject long password " do
+  	long = "a"*41
+  	hash =@attr.merge(:password => long,:password_conformation =>long)
+  	User.new(hash).should_not be_valid 
+  end
+  
   it "should require a name" do
    no_name_user = User.new(@attr.merge(:name => ""))
    no_name_user.should_not be_valid
@@ -56,4 +82,49 @@ describe User do
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
-end
+  
+  # a seperate describe block ch:7.3
+  #---------------------------------
+  describe "password encryption" do
+	before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+    
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+
+		#describe "block in side Pasword encryption"
+		describe "has_password? method" do
+		  it "should be true if the passwords match" do
+		    @user.has_password?(@attr[:password]).should be_true
+		  end    
+		  it "should be false if the passwords don't match" do
+		    @user.has_password?("invalid").should be_false
+		  end 
+		end # end of has_password ? method
+		
+		#describe bloc inside passwrd Encryption
+	 	describe "authenticate method" do
+
+		  it "should return nil on email/password mismatch" do
+		    wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+		    wrong_password_user.should be_nil
+		  end
+
+		  it "should return nil for an email address with no user" do
+		    nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+		    nonexistent_user.should be_nil
+		  end
+
+		  it "should return the user on email/password match" do
+		    matching_user = User.authenticate(@attr[:email], @attr[:password])
+		    matching_user.should == @user
+		  end
+		end # end of  authenticate method
+  end #end of "password encryption"
+end # end of User 
